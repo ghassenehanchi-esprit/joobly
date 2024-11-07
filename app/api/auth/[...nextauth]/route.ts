a7166c49696ec31c9257687657500257b12c1cbf
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-11-07 13:04:38 +0200

    auth fix 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,6 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import dbConnect from "@/database/dbConnect";
-import { User } from "@/models/User";
-import { compare } from "bcryptjs";
+import {authOptions} from "@/lib/authOptions";
+import NextAuth, {AuthOptions} from "next-auth";
 
-const authHandler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      id: "credentials",
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        await dbConnect().catch((error: any) => ({ error: "Connection Failed...!" }));
-        const email = credentials?.email as string;
-        const password = credentials?.password as string;
+const handler = NextAuth(authOptions as AuthOptions);
 
-        const user = await User.findOne({ email });
-        const passwordOk = user && (await compare(password, user.password));
-
-        return passwordOk ? user : null;
-      },
-    }),
-  ],
-});
-
-
-export { authHandler as GET, authHandler as POST };
+export { handler as GET, handler as POST }
---
+++ lib/authOptions.ts
@@ -0,0 +1,41 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import dbConnect from "@/database/dbConnect";
+import { User } from "@/models/User";
+import { compare } from "bcryptjs";
+import mongoose from "mongoose";
+
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      id: 'credentials',
+      credentials: {
+        username: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        await dbConnect().catch(error => { error: "Connection Failed...!"});
+        console.log(credentials);
+        const email = credentials?.email as string;
+        const password = credentials?.password as string;
+
+        mongoose.connect(process.env.MONGODB_URL as string);
+        const user = await User.findOne({email});
+        const passwordOk = user && await compare(password, user.password);
+
+        if (passwordOk) {
+          return user;
+        }
+
+        return null
+      }
+    })
+  ],
+};
+
