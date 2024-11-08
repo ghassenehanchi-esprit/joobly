61ba252e27ce2512033c577aa9e2c7b08596c0f1
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-11-08 18:47:40 +0200

    remake jobs page done
    tailwind styles added, positions changed, icons reset

--- lib/components/toolBar/topbar.tsx
+++ lib/components/toolBar/topbar.tsx
@@ -2,10 +2,11 @@
 import React, { useCallback } from "react";
 import Image from "next/image";
 import Dropdown from "@/lib/components/dropdown/dropdown";
-import styles from "./topBar.module.scss";
 import searchIcon from "@/public/images/icons/search-large-lens.svg";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { optionItems } from "@/lib/types/componentTypes";
+
+import { CiSearch } from "react-icons/ci";
 
 interface TopbarProps {
 	style?: React.CSSProperties;
@@ -47,62 +48,65 @@ const Topbar: React.FC<TopbarProps> = ({
 	);
 
 	return (
-		<div style={style} className={styles["jobs-topbar"]}>
-			<div className={styles["jobs-group"]}>
-				<Image src={searchIcon} alt='search' className={styles["jobs-icon"]} />
+		<div
+			style={style}
+			className=" bg-light rounded-lg shadow-[0_4px_120px_rgba(151,159,183,0.15)] py-4 px-6 min-w-[300px]"
+		>
+			<div className="flex items-center gap-3 mb-8">
+				<CiSearch className="w-5 h-5 text-gray-500"/>
 				<input
 					defaultValue={defaultJobSearchValue || ""}
 					onChange={(e) =>
 						router.push(pathname + "?" + createQueryString("jobTitle", e.target.value))
 					}
-					type='text'
-					className={styles["header-jobs"]}
-					placeholder='Search job name'
+					type="text"
+					className="w-[188px] h-[32px] placeholder-gray-500 placeholder-opacity-60 placeholder-font-semibold placeholder-line-[21px] text-lg"
+					placeholder="Search job name"
 				/>
 			</div>
 			{/*  @ts-ignore */}
 			<Dropdown
-				key='work-type-dropdown'
+				key="work-type-dropdown"
 				defaultSelected={defaultWorkType}
 				queryPushing={(label: string) =>
 					router.push(pathname + "?" + createQueryString("workType", label))
 				}
 				items={workType}
 				headerTitle={"Work Type"}
-				icon='/images/icons/findJob.svg'
+				icon="/images/icons/findJob.svg"
 			/>
 			{/*  @ts-ignore */}
 			<Dropdown
-				key='location-dropdown'
+				key="location-dropdown"
 				defaultSelected={defaultLocation}
 				queryPushing={(label: string) =>
 					router.push(pathname + "?" + createQueryString("location", label))
 				}
 				items={locations}
 				headerTitle={"Location"}
-				icon='/images/icons/location.svg'
+				icon="/images/icons/location.svg"
 			/>
 			{/*  @ts-ignore */}
 			<Dropdown
-				key='salary-dropdown'
+				key="salary-dropdown"
 				defaultSelected={defaultSalary}
 				queryPushing={(label: string) =>
 					router.push(pathname + "?" + createQueryString("salary", label))
 				}
 				items={salary}
 				headerTitle={"Salary"}
-				icon='/images/icons/dollar-circle.svg'
+				icon="/images/icons/dollar-circle.svg"
 			/>
 			{/*  @ts-ignore */}
 			<Dropdown
-				key='specialization-dropdown'
+				key="specialization-dropdown"
 				defaultSelected={defaultJobTitle}
 				queryPushing={(label: string) =>
 					router.push(pathname + "?" + createQueryString("jobTitle", label))
 				}
 				items={specializations}
 				headerTitle={"Specialization"}
-				icon='/images/icons/case.svg'
+				icon="/images/icons/case.svg"
 			/>
 		</div>
 	);
