4dfdc1ae3046547afa3841cd8cfb447dce17c9d5
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-11-15 17:32:11 +0200

    +

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
 import DashboardPage from "@/lib/components/dashboard/DashboardPage";
+import { getServerSession } from "next-auth";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+	title: "Joobly | Profile",
+	description: "user dashboard",
+};
 
 export default function Dashboard() {
+  const session = getServerSession();
+  console.log(session);
   return (
     <section className="mt-16 mb-20 px-4 min-h-[60%]">
         <DashboardPage/>
