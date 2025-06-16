24542db6d92c6f2cb70732943032abe83fbe9fad
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-06-16 14:54:55 +0300

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,25 @@
-"use client";
-import { Source_Sans_3 } from "next/font/google";
-import "@/lib/styles/globals.scss";
-import { Provider } from "react-redux";
-import store from "@/lib/store";
-import Footer from "@/app/footer";
-import { usePathname } from "next/navigation";
+"use client"
+import { Source_Sans_3 } from 'next/font/google';
+import '@/lib/styles/globals.scss';
+import ClientProviders from './providers';
+import Header from './header';
+import Footer from '@/app/footer';
+import { usePathname } from 'next/navigation';
 
-import { SessionProvider } from "next-auth/react";
-import Header from "./header";
-import { Metadata } from "next";
-
-const mainFont = Source_Sans_3({ subsets: ["latin"] });
-
+const mainFont = Source_Sans_3({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	const pathname = usePathname();
+    const pathname = usePathname();
 
-	return (
-		<html lang='en'>
-			<body className={mainFont.className}>
-				<SessionProvider>
-					{pathname === "/" && <Header needBackgroundHeader={false}/>}
-					<Provider store={store}>{children}</Provider>
-					{pathname !== "/" && <Footer />}
-				</SessionProvider>
-			</body>
-		</html>
-	);
+    return (
+	<html lang="en">
+        <body className={mainFont.className}>
+        <ClientProviders>
+            {pathname === "/" && <Header needBackgroundHeader={false} />}
+            {children}
+            {pathname !== "/" && <Footer />}
+        </ClientProviders>
+        </body>
+    </html>
+    );
 }
