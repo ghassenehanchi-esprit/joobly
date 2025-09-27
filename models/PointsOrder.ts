aa562c560bd7be6afcda41b7c129337404f7da19
Mohamed Ghassene Hanchi <124159272+ghassenehanchi-esprit@users.noreply.github.com>
2025-09-27 09:24:03 +0200

    Add session confirmation flow for Stripe checkout

--- models/PointsOrder.ts
+++ models/PointsOrder.ts
@@ -1,24 +1,27 @@
 import { Schema, model, models } from "mongoose";
 
 export type PointsOrderTypes = {
-    _id?: FormDataEntryValue;
-    userEmail: string;
-    title: string;
-    price: number;
-    points: number;
-    paymentType: string;
-    paid: boolean;
-    createdAt?: Date;
-  };
+  _id?: FormDataEntryValue;
+  userEmail: string;
+  title: string;
+  price: number;
+  points: number;
+  paymentType: string;
+  paid: boolean;
+  createdAt?: Date;
+};
 
-const PointsOrderSchema = new Schema({
-    userEmail: {type: String, required: true},
-    title: {type: String, required: true},
-	price: {type: Number, required: true},
-	points: {type: Number, required: true},
-    paymentType: {type: String},
-    paid: {type: Boolean, default: false},
-}, {timestamps: true});
+const PointsOrderSchema = new Schema(
+  {
+    userEmail: { type: String, required: true },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    points: { type: Number, required: true },
+    paymentType: { type: String },
+    paid: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-
-export const PointsOrder = models?.PointsOrder || model('PointsOrder', PointsOrderSchema);
+export const PointsOrder =
+  models?.PointsOrder || model("PointsOrder", PointsOrderSchema);
